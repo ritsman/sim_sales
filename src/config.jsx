7baaf4a287ab1c89718585ccb8601df83d66a364
Ritesh Mangwani <ritsman@gmail.com>
2025-03-14 19:17:25 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,10 +1,11 @@
 
 
 const config = {
-  API_URL: "http://localhost:3000",
+  API_URL: "https://s1.simranwebsoft.com/aysales",
 };
 
 export default config;
 
 //https://a3.arya-erp.in/api2/aryapi
 //http://localhost:3001
+//https://s1.simranwebsoft.com/aysales
