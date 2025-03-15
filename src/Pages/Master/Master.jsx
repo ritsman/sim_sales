33479b8564787d2bb34b057ffa5f690a34e0e629
Ritesh Mangwani <ritsman@gmail.com>
2025-03-15 17:36:33 +0530

    schedular

--- src/Pages/Master/Master.jsx
+++ src/Pages/Master/Master.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { Navigate, Outlet } from "react-router-dom";
-import { ProgressBarComponent } from "@syncfusion/ej2-react-progressbar";
+//import { ProgressBarComponent } from "@syncfusion/ej2-react-progressbar";
 import "../../css/Master/master.css";
-import { registerLicense } from "@syncfusion/ej2-base";
 
-import "../../css/Master/master-common.css";
-registerLicense(
-  "Ngo9BigBOggjHTQxAR8/V1NAaF1cXmhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEFjW39WcHJQRmNaUEx/WQ=="
-);
 
 export default function Master() {
   const { auth, setAuth, userDetails } = useAuth();
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthProvider";
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXxfdXRdRmJeVkZ3XUo=');
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  // </React.StrictMode>
+   </React.StrictMode>
 );
