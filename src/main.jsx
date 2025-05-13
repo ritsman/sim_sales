11624e2b58f589f95eb95bb6c3c846242d741a91
Ritesh Mangwani <ritsman@gmail.com>
2025-05-13 16:40:39 +0530

    withWO

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import { ToastContainer } from "react-toastify";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthProvider";
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXxcdnRRRmddUUNxXUZWYUA="
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
